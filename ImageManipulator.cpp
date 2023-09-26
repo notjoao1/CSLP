58e76f01ec4c24400ee2b6318db86390e508c3ba
uTigas <tfgp@ua.pt>
2023-09-26 18:29:54 +0100

    Add OpenCV to project

---
+++ ImageManipulator.cpp
@@ -0,0 +1,6 @@
+//
+// Created by tiago on 26-09-2023.
+//
+
+#include "ImageManipulator.h"
+
---
+++ ImageManipulator.h
@@ -0,0 +1,14 @@
+//
+// Created by tiago on 26-09-2023.
+//
+
+#ifndef GTD_VC_IMAGEMANIPULATOR_H
+#define GTD_VC_IMAGEMANIPULATOR_H
+
+
+class ImageManipulator {
+
+};
+
+
+#endif //GTD_VC_IMAGEMANIPULATOR_H
