23fe02051aff3648dbb31be7753da334c30c2832
notjoao1 <joao.dourado1@ua.pt>
2023-09-27 19:42:44 +0100

    [Add] Abstract 'Effect' class for low level image operations

---
+++ src/Effect.h
@@ -0,0 +1,19 @@
+//
+// Created by JPe_D on 9/27/2023.
+//
+
+#ifndef GTD_VC_EFFECT_H
+#define GTD_VC_EFFECT_H
+
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+
+// abstract class for effects to apply to each frame
+class Effect {
+public:
+    virtual Mat apply(Mat frame) = 0;
+};
+
+
+#endif //GTD_VC_EFFECT_H
