14c04cbb256c14e93e8b71e21a17b110e646155e
notjoao1 <joao.dourado1@ua.pt>
2023-10-04 14:33:00 +0100

    [Fix] BGR indexing + prevent grayscaling single-channel images

--- src/Grayscale.cpp
+++ src/Grayscale.cpp
@@ -8,8 +8,11 @@
 
 using namespace cv;
 
-// todo: testar isto para YUV, RGBA, etc etc
 Mat Grayscale::apply(Mat frame) {
+    if (frame.channels() != 3) {
+        std::cerr << "Grayscale::apply() ERROR: invalid input frame - must have 3 channels. (Frame unaltered)" << std::endl;
+        return frame;
+    }
     Mat channel[3];
     split(frame, channel);
 
@@ -22,9 +25,9 @@ Mat Grayscale::apply(Mat frame) {
     uchar curr_r, curr_g, curr_b;
     for(int i = 0; i < frame.rows; i++) {
         for (int j = 0; j < frame.cols; j++) {
-            curr_r = channel[0].at<uchar>(i, j);
+            curr_b = channel[0].at<uchar>(i, j);
             curr_g = channel[1].at<uchar>(i, j);
-            curr_b = channel[2].at<uchar>(i, j);
+            curr_r = channel[2].at<uchar>(i, j);
             return_frame.at<uchar>(i, j) = weight_r * curr_r + weight_g * curr_g + weight_b * curr_b;
         }
     }
