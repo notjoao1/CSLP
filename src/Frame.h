5f6b4b26626664fbbc2857a0494f8d06e0087a34
uTigas <tfgp@ua.pt>
2023-10-22 13:20:03 +0100

    Doxygen doc and testing.

--- src/Frame.h
+++ src/Frame.h
@@ -14,19 +14,55 @@
 using namespace std;
 using namespace cv;
 
+/**
+ * @class Frame
+ * @brief A class for storing and allowing manipulation of Image Frames.
+ */
 class Effect;
 class Frame {
 private:
+
     Channels channels;
     vector<Effect*>effects;
 public:
+    /**
+     * @brief Adds effect to Frame's effect list.
+     * @param effect Effect to be added.
+     */
     void addEffect(Effect* effect);
+    /**
+     * @brief Applies to Frame all effects in Frame's effect list.
+     */
     void applyEffects();
+    /**
+     * @brief Returns Frame Channels.
+     * returns Channels of Frame.
+     */
     Channels getChannels();
+    /**
+     * @brief Sets the Color Space of Frame.
+     * @param color Color Space to be set.
+     */
     void setColorSpace(Color color);
+    /**
+     * @brief Converts Color Space of Frame.
+     * @param dest Final intended Color Space.
+     */
     void convertColorSpace(Color dest);
+    /**
+     * @brief Get frame in Matrix format.
+     * @returns Frame in OpenCV's Matrix format.
+     */
     Mat getFrame();
+    /**
+     * @brief Sets Frame Image from Matrix.
+     * @param mat Source matrix to be set to Frame.
+     */
     void fromMat(Mat mat);
+    /**
+     * @brief Creates Frame object from Channels object.
+     * @param c Channels object to be used as Frame base.
+     */
     Frame(Channels c);
 };
 
--- src/ImageManipulator.h
+++ src/ImageManipulator.h
@@ -11,11 +11,26 @@
 using namespace std;
 using namespace cv;
 
-
+/**
+* @class BitStreamRead
+* @brief A class for reading/writings images from/to a file.
+*/
 class ImageManipulator {
 private:
 public:
+     /**
+     * @brief Reads Image from file.
+     * @param file_name String defining the path to the to be written file.
+     * @returns Matrix read from file.
+     */
     Mat ReadImage(string file_name);
+     /**
+     * @brief Writes Image to file.
+     * @param image Image contained in a OpenCV Matrix object.
+     * @param file_name String defining the path to the to be written file.
+     * @returns True if write has been successful. False otherwise.
+     */
+
     bool WriteImage(string file_name, Mat image);
 };
 
