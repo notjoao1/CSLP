f6748368101bb0de57d7e19d3835a22fbb5a3f9f
notjoao1 <joao.dourado1@ua.pt>
2023-10-22 14:44:42 +0100

    [Documentation] Docs for Threshold, VideoManipulator and WatermarkImage classes

--- src/Threshold.h
+++ src/Threshold.h
@@ -8,12 +8,30 @@
 
 #include "Effect.h"
 
+/**
+ * @class Threshold
+ * @brief Class used apply threshold based segmentation on an image.
+ */
 class Threshold: public Effect{
 private:
-    unsigned char threshold;
+    unsigned char threshold; /**< Threshold for pixel values. */
+
+    /**< Boolean variable to define the way 'threshold' will be applied in the algorithm.
+     * If false, pixel values lower than 'threshold' will be interpreted as black (0) and
+     * values higher will be interpreted as white (255). If true, the opposite will occur. */
     bool inverted;
 public:
+
+    /**
+      * @brief Constructor for Threshold class.
+      * @param threshold Threshold for pixel values
+      * @param inverted Invert the pixel output values during application of algorithm
+      */
     Threshold(unsigned char threshold,bool inverted);
+
+    /**
+      * @brief Apply image segmentation algorithm based on threshold with given values.
+      */
     void apply(Frame* frame) override;
 };
 
