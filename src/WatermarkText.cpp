c88508b27d02df72fa2dab9c4a5e982881365426
notjoao1 <joao.dourado1@ua.pt>
2023-10-22 14:40:57 +0100

    [Refactor] WatermarkText color support (black and white) + documentation

--- src/WatermarkText.cpp
+++ src/WatermarkText.cpp
@@ -10,11 +10,13 @@ using namespace std;
 
 void WatermarkText::apply(Frame* frame) {
     Mat frameMat=frame->getChannels().toMat();
-    // todo: custom coordinates, color (?), and fontScale (?)
-    cv::putText(frameMat, this->text,cv::Point(20, frameMat.rows - 30),cv::FONT_HERSHEY_DUPLEX,2,cv::Scalar(0,0,0),2,false);
+    cv::putText(frameMat, this->text,cv::Point(20, frameMat.rows - 30),cv::FONT_HERSHEY_DUPLEX,2,cv::Scalar(this->color,this->color,this->color),2,false);
     frame->fromMat(frameMat);
 }
 
-WatermarkText::WatermarkText(string text) {
+
+
+WatermarkText::WatermarkText(const string& text, TextColor color) {
     this->text = text;
+    this->color = color;
 }
--- src/WatermarkText.h
+++ src/WatermarkText.h
@@ -11,11 +11,38 @@
 
 using namespace std;
 
+/**< Available colors for watermark text */
+enum TextColor {
+    BLACK = 0,
+    WHITE = 255
+};
+
+/**
+ * @class WatermarkText
+ * @brief Class used to apply watermark text onto an image.
+ */
 class WatermarkText : public Effect {
 private:
+    /**< Watermark image to be applied onto a frame. */
     string text;
+
+    /**< Color of watermark text. Defined in enum inside of WatermarkText.h */
+    TextColor color;
 public:
-    WatermarkText(string text);
+
+    /**
+      * @brief Constructor for WatermarkText class.
+      * @param text String watermark to be applied.
+      * @param color Color for watermark text.
+
+      */
+    WatermarkText(const string& text, TextColor color);
+
+    /**
+      * @brief Apply watermark text onto input 'frame'.
+      * By default, will apply watermark in bottom corner.
+      * @param frame Frame in which the watermark image will be applied.
+      */
     void apply(Frame* frame) override;
 };
 
