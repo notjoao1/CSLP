0b27cdb9d678316b4d2f415ab3779531617e3cb5
uTigas <tfgp@ua.pt>
2023-10-31 19:03:46 +0000

    [ADD] Codec refinement

--- src/BitStream/BitStreamWrite.h
+++ src/BitStream/BitStreamWrite.h
@@ -47,7 +47,7 @@ public:
      * @brief Constructor for the BitStreamWrite class.
      * @param filename The name of the file to write to.
      */
-    explicit BitStreamWrite(const std::string& filename) {
+     BitStreamWrite(const std::string& filename) {
         this->file.open(filename, std::ios::binary | std::ios::out);
     }
 
